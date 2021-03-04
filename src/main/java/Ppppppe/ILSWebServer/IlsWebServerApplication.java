8bc8e16e1352eeb23d54696bba07195efdf85c27
Denis Pesterov <dspesterov@gmail.com>
2021-03-04 14:39:11 +0300

    spring boot starter + dependencies

---
+++ src/main/java/Ppppppe/ILSWebServer/IlsWebServerApplication.java
@@ -0,0 +1,13 @@
+package Ppppppe.ILSWebServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlsWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlsWebServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ppppppe/ILSWebServer/IlsWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Ppppppe.ILSWebServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlsWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
